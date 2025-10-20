f1d3fc26d46183ccc058f6df9cb818d99547cd54
RoriLaLoutre <robin2.gillot2@gmail.com>
2025-10-20 14:25:37 +0200

    initial commit

---
+++ app/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="min-h-screen bg-red-500 flex items-center justify-center">
+    <div class="bg-white p-8 rounded-lg shadow-xl">
+      <h1 class="text-3xl font-bold text-blue-600">Test Tailwind</h1>
+      <p class="text-gray-600 mt-4">Si vous voyez du rouge en fond, Tailwind fonctionne !</p>
+    </div>
+  </div>
+</template>
