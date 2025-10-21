e69d47ae36501d3d36944af15f2eac831887dca1
Mathis <mathislegamin@gmail.com>
2025-10-21 14:50:48 +0200

    hotfix/merge

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -17,9 +17,10 @@
     </div>
   </div>
 
-<div v-if="showModal" class="fixed inset-0 flex items-center justify-center z-50" @click.self="closeModal">
-  <div v-if="modalType === 'avatar'" class="bg-white rounded-xl p-8 w-[96] shadow-lg text-center relative">
-    <div class="bg-white border rounded-xl p-6 w-[32rem] shadow-lg text-center relative">
+  <div v-if="showModal" class="fixed inset-0 flex items-center justify-center z-50" @click.self="closeModal">
+    
+    <!-- Modal AVATAR -->
+    <div v-if="modalType === 'avatar'" class="bg-white border rounded-xl p-6 w-[32rem] shadow-lg text-center relative">
       <div class="flex w-full justify-between items-center border-b pb-2 mb-4">
         <h2 class="text-2xl font-bold text-brand-navy">Chat with {{ selectedAvatar }}</h2>
         <UButton icon="i-lucide-x" variant="ghost" color="primary" @click="closeModal" />
@@ -29,14 +30,9 @@
         <div class="flex-1 overflow-y-auto p-4">
           <UChatMessages
             :messages="messages"
-            :assistant="{
-            variant: 'solid',
-            }"
-            :user="{
-                variant: 'solid',
-                side: 'right',
-            }"
-        />
+            :assistant="{ variant: 'solid' }"
+            :user="{ variant: 'solid', side: 'right' }"
+          />
         </div>
 
         <div class="p-3 border-t flex gap-2 items-center">
@@ -49,54 +45,54 @@
           <UButton color="primary" @click="sendMessage">Send</UButton>
           <UButton icon="i-lucide-arrow-up-from-line" color="primary" @click="sendMessage" />
         </div>
-      </div>    
-  </div>
+      </div>
+    </div>
 
-  <div
-  v-if="modalType === 'folder'"
-  class="relative bg-[#1f1f1f] p-4 rounded-xl shadow-lg w-[1000px] h-[650px]"
->
-  <UButton
-    icon="i-lucide-x"
-    class="absolute top-4 right-4 z-10"
-    @click="closeModal"
-  />
-  <img
-    src="/Group 3.png"
-    alt="Dossier d'enquête"
-    class="absolute inset-0 w-full h-full object-contain pointer-events-none select-none"
-  />
-  <div class="absolute top-[15%] left-[20%] w-[25%] h-[40%] overflow-y-auto">
-    <p class="w-full h-full bg-transparent text-black resize-none outline-none p-2">
-      C’est une belle après-midi d’été au parc.
-      Tout le monde fait la sieste après avoir bien joué…
-      Mais quand Capucine se réveille, sa boîte à billes bleue a disparu !
-      Quelqu’un l’a sûrement prise pendant que tout le monde dormait.
-      <br>
-      <br>
-      À toi de mener l’enquête : interroge les enfants, écoute leurs souvenirs, et découvre qui a volé les billes de Capucine !
-    </p>
+    <!-- Modal FOLDER -->
+    <div
+      v-if="modalType === 'folder'"
+      class="relative bg-[#1f1f1f] p-4 rounded-xl shadow-lg w-[1000px] h-[650px]"
+    >
+      <UButton
+        icon="i-lucide-x"
+        class="absolute top-4 right-4 z-10"
+        @click="closeModal"
+      />
+      <img
+        src="/Group 3.png"
+        alt="Dossier d'enquête"
+        class="absolute inset-0 w-full h-full object-contain pointer-events-none select-none"
+      />
+      <div class="absolute top-[15%] left-[20%] w-[25%] h-[40%] overflow-y-auto">
+        <p class="w-full h-full bg-transparent text-black resize-none outline-none p-2">
+          C’est une belle après-midi d’été au parc.
+          Tout le monde fait la sieste après avoir bien joué…
+          Mais quand Capucine se réveille, sa boîte à billes bleue a disparu !
+          Quelqu’un l’a sûrement prise pendant que tout le monde dormait.
+          <br><br>
+          À toi de mener l’enquête : interroge les enfants, écoute leurs souvenirs, et découvre qui a volé les billes de Capucine !
+        </p>
+      </div>
+      <div class="absolute bottom-[17%] left-[17%] w-[30%] h-[19%] overflow-y-auto">
+        <p class="w-full h-full bg-transparent text-black resize-none outline-none p-2">
+          Indice:
+          <br>
+          D’après les premiers témoignages, quelqu’un aurait aperçu une silhouette
+          bouger près du bac à sable pendant la sieste.
+          <br>
+          Il paraît que <span class="font-bold">Sophie</span> n’a pas dormi et dessinait pendant ce temps-là.  
+          Elle a peut-être remarqué quelque chose d’important.
+        </p>
+      </div>
+      <div class="absolute top-[15%] right-[20%] w-[25%] h-[70%]">
+        <textarea
+          v-model="folderText"
+          class="w-full h-full bg-transparent text-black resize-none outline-none p-2"
+          placeholder="Entrez vos notes"
+        ></textarea>
+      </div>
+    </div>
   </div>
-  <div class="absolute bottom-[17%] left-[17%] w-[30%] h-[19%] overflow-y-auto">
-    <p class="w-full h-full bg-transparent text-black resize-none outline-none p-2">
-      Indice:
-      <br>
-      D’après les premiers témoignages, quelqu’un aurait aperçu une silhouette
-      bouger près du bac à sable pendant la sieste.
-      <br>
-      Il paraît que <span class="font-bold">Sophie</span> n’a pas dormi et dessinait pendant ce temps-là.  
-      Elle a peut-être remarqué quelque chose d’important.
-    </p>
-  </div>
-  <div class="absolute top-[15%] right-[20%] w-[25%] h-[70%]">
-    <textarea
-      v-model="folderText"
-      class="w-full h-full bg-transparent text-black resize-none outline-none p-2"
-      placeholder="Entrez vos notess"
-    ></textarea>
-  </div>
-</div>
-</div>
 </template>
 
 <script setup lang="ts">
@@ -156,9 +152,10 @@ function sendMessage() {
   }, 500)
 }
 </script>
+
 <style scoped>
 ::v-deep(article[data-role="user"] > div) {
-  justify-content: flex-end; /* pousse le container à droite */
+  justify-content: flex-end;
   text-align: right;  
 }
 ::v-deep(article[data-role="user"] > div > div) {
@@ -168,7 +165,7 @@ function sendMessage() {
   color: white; 
 }
 ::v-deep(article[data-role="assistant"] > div) {
-  justify-content: flex-start; /* pousse le container à gauche */
+  justify-content: flex-start;
   text-align: left;  
 }
 ::v-deep(article[data-role="assistant"] > div > div) {
